a79a6f0eac70c2ae314867dbda38dfa64b20452d
BrendaPenabaz <84151341+BrendaPenabaz@users.noreply.github.com>
2021-07-28 17:36:12 -0500

    Update t_control_pago.sql

--- t_control_pago.sql
+++ t_control_pago.sql
@@ -10,9 +10,9 @@ create trigger t_control_pago
 	select @cliente=inserted.cod_cliente, @contrato=inserted.cod_contrato, @monto=inserted.monto,@fecha=inserted.fecha
 		from inserted
 
-	if exists(select [cod_contrato] from [dbo].[Contrarto] where [cod_cliente]=@cliente and [cod_plan]=@contrato)
+	if exists(select [cod_contrato] from [dbo].[Contrarto] where [cod_cliente]=@cliente and [cod_contrato]=@contrato)
 	begin 
-		if exists(select [monto] from [dbo].[Plan] where [monto]=@monto and [cod_plan]=@contrato)
+		if exists(select [monto] from [dbo].[Plan] where [monto]=@monto )
 		begin
 			insert into [dbo].[Pago]	
 			values(@cliente,@contrato,@monto,@fecha)
